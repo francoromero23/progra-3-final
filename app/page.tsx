a5c3938345e854975733f5c73dacb75c2d6cbc28
Franco <franco.romero@uap.edu.ar>
2025-03-02 19:56:12 -0300

    estilos a el login y register

--- app/page.tsx
+++ app/page.tsx
@@ -2,33 +2,34 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 text-gray-800 p-8">
-      <div className="max-w-4xl w-full bg-white p-8 rounded-lg shadow-lg">
-        <h1 className="text-4xl font-semibold text-center text-blue-700 mb-6">
-          Bienvenido a las Noticias Corporativas
-        </h1>
-        <p className="text-lg text-center text-gray-600 mb-8">
-          Inicia sesión o regístrate para acceder a las últimas noticias y
-          actualizaciones de nuestra empresa.
-        </p>
-        <div className="flex justify-center space-x-6">
-          <Link
-            href="/auth/login"
-            className="bg-blue-600 text-white text-lg py-3 px-6 rounded-lg shadow-md hover:bg-blue-700 transition-all"
-          >
-            Iniciar Sesión
-          </Link>
-          <Link
-            href="/auth/register"
-            className="bg-green-600 text-white text-lg py-3 px-6 rounded-lg shadow-md hover:bg-green-700 transition-all"
-          >
-            Registrarse
-          </Link>
+    <div className="min-h-screen relative overflow-hidden">
+      <div className="absolute inset-0 bg-[url('/fondo_principal.webp')] bg-cover bg-center opacity-90"></div>
+
+      <div className="flex flex-col items-center justify-center min-h-screen relative z-10">
+        <div className="bg-white bg-opacity-10 backdrop-blur-lg p-16 rounded-xl shadow-lg w-full max-w-4xl text-white text-center">
+          <h1 className="text-4xl font-bold mb-6">
+            Bienvenido a las Noticias Corporativas
+          </h1>
+          <p className="text-lg mb-8">
+            Inicia sesión o regístrate para acceder a las últimas noticias y
+            actualizaciones de nuestra empresa.
+          </p>
+          <div className="flex justify-center space-x-6">
+            <Link
+              href="/auth/login"
+              className="bg-blue-600 bg-opacity-80 hover:bg-opacity-100 text-white text-lg py-3 px-6 rounded-lg shadow-md transition-all"
+            >
+              Iniciar Sesión
+            </Link>
+            <Link
+              href="/auth/register"
+              className="bg-green-600 bg-opacity-80 hover:bg-opacity-100 text-white text-lg py-3 px-6 rounded-lg shadow-md transition-all"
+            >
+              Registrarse
+            </Link>
+          </div>
         </div>
       </div>
-      <footer className="mt-12 text-center text-gray-500 text-sm">
-        <p>&copy; 2025 FranSOFT. Todos los derechos reservados.</p>
-      </footer>
     </div>
   );
 }
